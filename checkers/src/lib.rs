8eb202878f5d25ff932b3c986d548fc93c4e053f
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2024-12-09 06:30:23 -0500

    Added components to represent checkers and added camera setup

--- checkers/src/lib.rs
+++ checkers/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Component)]
+pub enum CheckerColor {
+    Black,
+    Red,
+}
+
+#[derive(Component)]
+pub struct Checker;
+
+#[derive(Component)]
+pub struct Board {
+    pub grid: [[Option<Entity>; 8]; 8],
+}
+
+#[derive(Component)]
+pub enum Piece {
+    King,
+    Regular,
+}
+
+pub fn camera_setup(mut commands: Commands) {
+    commands.spawn((
+        Camera {
+            clear_color: ClearColorConfig::Custom(Color::Srgba(Srgba::BLUE)),
+            ..Default::default()
+        },
+        Camera2d,
+        OrthographicProjection {
+            scaling_mode: bevy::render::camera::ScalingMode::AutoMin {
+                min_width:  128.0,
+                min_height: 128.0,
+            },
+            ..OrthographicProjection::default_2d()
+        },
+    ));
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}

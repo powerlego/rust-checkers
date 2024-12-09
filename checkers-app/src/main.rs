8e9d726cbe3a292edddadc3139d258bcf6fce828
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2024-12-09 06:29:35 -0500

    Added app startup

--- checkers-app/src/main.rs
+++ checkers-app/src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+use checkers::camera_setup;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("Checkers"),
+                        resolution: (640.0, 640.0).into(),
+                        resizable: false,
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .build(),
+        )
+        .add_systems(Startup, camera_setup)
+        .run();
 }
